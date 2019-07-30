86f0950957781f14d9acae7354f061518c73986b
Phuong Minh <phuongminh2303@gmail.com>
2019-07-30 23:04:34 +0700

    Set up Messages section

---
+++ src/components/Messages/MessageForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Segment, Button, Input } from 'semantic-ui-react';
+
+class MessageForm extends React.Component {
+  render() {
+    return (
+      <Segment className="message__form">
+        <Input
+          fluid
+          name="message"
+          style={{ marginBotton: '0.7rem' }}
+          label={<Button icon={'add'} />}
+          labelPosition="left"
+          placeholder="write your message"
+        />
+        <Button.Group icon widths="2">
+          <Button
+            color="orange"
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+          />
+          <Button
+            color="teal"
+            content="Upload Media"
+            labelPosition="right"
+            icon="cloud upload"
+          />
+        </Button.Group>
+      </Segment>
+    );
+  }
+}
+
+export default MessageForm;
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Segment, Comment } from 'semantic-ui-react';
+import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
 
 class Messages extends React.Component {
   render() {
     return (
-    <div>
-      Messages
-    </div>
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+        <MessageForm />
+      </React.Fragment>
     );
   }
 }
