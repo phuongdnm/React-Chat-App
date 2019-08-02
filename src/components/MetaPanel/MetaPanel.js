4b09d3b3b472722541b46abaa9b2cb99de152c76
Phuong Minh <phuongminh2303@gmail.com>
2019-08-02 11:20:14 +0700

    Set up MetaPanel component

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,11 +1,70 @@
 import React from 'react';
+import { Segment, Accordion, Header, Icon } from 'semantic-ui-react';
 
 class MetaPanel extends React.Component {
+  state = {
+    activeIndex: 0,
+    isPrivateChannel: this.props.isPrivateChannel
+  };
+
+  setActiveIndex = (event, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
+    const { activeIndex, isPrivateChannel } = this.state;
+
+    if(isPrivateChannel) return null;
+
     return (
-    <div>
-      Meta Panel
-    </div>
+      <Segment>
+        <Header as="h3" attached="top">
+          About # channel
+        </Header>
+        <Accordion styled attached="true">
+          <Accordion.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="info" />
+            Channel Details
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            details
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="user circle" />
+            Top Posters
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            posters
+          </Accordion.Content>
+
+          <Accordion.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name="dropdown" />
+            <Icon name="pencil alternate" />
+            Created By
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 2}>
+            cretor
+          </Accordion.Content>
+        </Accordion>
+      </Segment>
     );
   }
 }
