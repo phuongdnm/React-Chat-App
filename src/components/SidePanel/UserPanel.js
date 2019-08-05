e93ad770457daf8d7e59b54a2a245bc0e061b997
Phuong Minh <phuongminh2303@gmail.com>
2019-08-05 20:31:44 +0700

    Firebase database rules and storage rules

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -93,7 +93,7 @@ class UserPanel extends React.Component {
   uploadCroppedImage = () => {
     const { storageRef, userRef, blob, metadata } = this.state;
     storageRef
-      .child(`avatars/user-${userRef.uid}`)
+      .child(`avatars/user/${userRef.uid}`)
       .put(blob, metadata)
       .then(snap => {
         snap.ref.getDownloadURL().then(downloadURL => {
